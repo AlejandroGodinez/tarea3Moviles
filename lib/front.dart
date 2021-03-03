84bcfb26ee749fbc282130cd2c9af470ac977e8c
AlejandroGodinez <is714181@iteso.mx>
2021-03-03 12:34:03 -0600

    Appbar added title

--- lib/front.dart
+++ lib/front.dart
@@ -14,6 +14,7 @@ class _FrontState extends State<Front> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          title: Text('Guess the word!', style: TextStyle(color: Colors.white),),
           backgroundColor: Colors.green,
         ),
         body: BlocProvider(
